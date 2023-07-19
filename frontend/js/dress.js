254a4d06871e0c41ee889877f52c3c69e09095b9
Jesse van der Merwe <jesse@bbd.co.za>
2023-07-19 10:59:42 +0200

    Added start of login screen

--- frontend/js/dress.js
+++ frontend/js/dress.js
@@ -7,38 +7,48 @@ const dressTop = document.getElementById("dress-top");
 const dressSkirt = document.getElementById("dress-skirt");
 
 let topCounter = 0;
-const topArray = new Array("./images/tops/top_asymmetric.png", "./images/tops/top_scoop.png", "./images/tops/top_sweetheart.png", "./images/tops/top_vneck.png")
+const topArray = new Array(
+  "./images/tops/top_asymmetric.png",
+  "./images/tops/top_scoop.png",
+  "./images/tops/top_sweetheart.png",
+  "./images/tops/top_vneck.png"
+);
 let skirtCounter = 0;
-const skirtArray = new Array("./images/skirts/skirt_ballgown.png", "./images/skirts/skirt_line.png", "./images/skirts/skirt_mermaid.png", "./images/skirts/skirt_trumpet.png")
+const skirtArray = new Array(
+  "./images/skirts/skirt_ballgown.png",
+  "./images/skirts/skirt_line.png",
+  "./images/skirts/skirt_mermaid.png",
+  "./images/skirts/skirt_trumpet.png"
+);
 
 arrowTopLeft.addEventListener("click", () => {
-    topCounter++;
-    if(topCounter >= topArray.length){
-        topCounter = 0;
-    }
-    dressTop.src = topArray[topCounter];    
+  topCounter++;
+  if (topCounter >= topArray.length) {
+    topCounter = 0;
+  }
+  dressTop.src = topArray[topCounter];
 });
 
 arrowTopRight.addEventListener("click", () => {
-    topCounter--;
-    if(topCounter < 0){
-        topCounter = topArray.length-1;
-    }
-    dressTop.src = topArray[topCounter];    
+  topCounter--;
+  if (topCounter < 0) {
+    topCounter = topArray.length - 1;
+  }
+  dressTop.src = topArray[topCounter];
 });
 
 arrowSkirtLeft.addEventListener("click", () => {
-    skirtCounter++;
-    if(skirtCounter >= skirtArray.length){
-        skirtCounter = 0;
-    }
-    dressSkirt.src = skirtArray[skirtCounter];    
+  skirtCounter++;
+  if (skirtCounter >= skirtArray.length) {
+    skirtCounter = 0;
+  }
+  dressSkirt.src = skirtArray[skirtCounter];
 });
 
 arrowSkirtRight.addEventListener("click", () => {
-    skirtCounter--;
-    if(skirtCounter < 0){
-        skirtCounter = skirtArray.length-1;
-    }
-    dressSkirt.src = skirtArray[skirtCounter];    
+  skirtCounter--;
+  if (skirtCounter < 0) {
+    skirtCounter = skirtArray.length - 1;
+  }
+  dressSkirt.src = skirtArray[skirtCounter];
 });
