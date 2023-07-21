b19b33de0eda1c977333654d9a0f5bd846874ce3
Mmasehume Raphiri <mmasehume@bbd.co.za>
2023-07-21 21:59:21 +0200

    database script

---
+++ database/SayYesToTheDress.sql
@@ -0,0 +1,162 @@
+IF EXISTS(select * from sys.databases where name='SayYesToTheDressDB')
+DROP DATABASE SayYesToTheDressDB
+
+CREATE DATABASE SayYesToTheDressDB;
+GO
+
+USE SayYesToTheDressDB;
+GO
+
+CREATE TABLE [User] (
+  [userID] [int] IDENTITY(1,1) NOT NULL,
+  [username] [nvarchar](255) NOT NULL,
+  CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+	(
+		[userID] ASC
+	)
+)
+GO
+
+CREATE TABLE [Dress] (
+  [dressID] [int] IDENTITY(1,1) NOT NULL,
+  [userID] [int] NOT NULL,
+  [skirtID] [int],
+  [topID] [int],
+  [veilID] [int],
+  [shoesID] [int],
+  [sleeveID] [int],
+  CONSTRAINT [PK_Dress] PRIMARY KEY CLUSTERED 
+	(
+		[dressID] ASC
+	)
+)
+GO
+
+CREATE TABLE [Skirts] (
+  [skirtID] [int] IDENTITY(1,1) NOT NULL,
+  [skirtName] [varchar](255) NOT NULL,
+   CONSTRAINT [PK_Skirt] PRIMARY KEY CLUSTERED 
+	(
+		[skirtID] ASC
+	)
+)
+GO
+
+CREATE TABLE [Tops] (
+  [topID] [int] IDENTITY(1,1) NOT NULL,
+  [topName] [varchar](255) NOT NULL,
+   CONSTRAINT [PK_Top] PRIMARY KEY CLUSTERED 
+	(
+		[topID] ASC
+	)
+)
+GO
+
+CREATE TABLE [Veils] (
+  [veilID] [int] IDENTITY(1,1) NOT NULL,
+  [veilName] [varchar](255) NOT NULL,
+   CONSTRAINT [PK_Veil] PRIMARY KEY CLUSTERED 
+	(
+		[veilID] ASC
+	)
+)
+GO
+
+CREATE TABLE [Shoes] (
+  [shoeID] [int] IDENTITY(1,1) NOT NULL,
+  [shoeName] [varchar](255) NOT NULL,
+   CONSTRAINT [PK_Shoes] PRIMARY KEY CLUSTERED 
+	(
+		[shoeID] ASC
+	)
+)
+GO
+
+CREATE TABLE [Sleeves] (
+  [sleeveID] [int] IDENTITY(1,1) NOT NULL,
+  [sleeveName] [varchar](255) NOT NULL,
+   CONSTRAINT [PK_Sleeves] PRIMARY KEY CLUSTERED 
+	(
+		[sleeveID] ASC
+	)
+)
+GO
+
+ALTER TABLE [Dress] ADD FOREIGN KEY ([userID]) REFERENCES [User] ([userID])
+GO
+
+ALTER TABLE [Dress] ADD FOREIGN KEY ([skirtID]) REFERENCES [Skirts] ([skirtID])
+GO
+
+ALTER TABLE [Dress] ADD FOREIGN KEY ([topID]) REFERENCES [Tops] ([topID])
+GO
+
+ALTER TABLE [Dress] ADD FOREIGN KEY ([veilID]) REFERENCES [Veils] ([veilID])
+GO
+
+ALTER TABLE [Dress] ADD FOREIGN KEY ([shoesID]) REFERENCES [Shoes] ([shoeID])
+GO
+
+ALTER TABLE [Dress] ADD FOREIGN KEY ([sleeveID]) REFERENCES [Sleeves] ([sleeveID])
+GO
+
+--- ****************************************************************  DRESS ITEMS DATA  **********************************************************************
+INSERT INTO [Skirts]
+           ([skirtName])
+VALUES	('ballgown'),
+        ('line'),
+        ('mermaid'),
+        ('trumpet')
+GO
+
+INSERT INTO [Tops]
+           ([topName])
+VALUES	('asymmetric'),
+        ('scoop'),
+        ('sweetheart'),
+        ('vneck')
+GO
+
+INSERT INTO [Veils]
+           ([veilName])
+VALUES	('long'),
+		    ('short')
+GO
+
+INSERT INTO [Shoes]
+           ([shoeName])
+VALUES	('heels'),
+		    ('pumps')
+GO
+
+INSERT INTO [Sleeves]
+           ([sleeveName])
+VALUES	('offSholder')
+GO
+
+--- ****************************************************************  INSERT DUMMY USER **********************************************************************
+INSERT INTO [User]
+           ([username])
+VALUES	('dummy')
+GO
+INSERT INTO [User]
+           ([username])
+VALUES	('test user')
+GO
+INSERT INTO [User]
+           ([username])
+VALUES	('dummy user')
+GO
+
+--- ****************************************************************  INSERT DUMMY USER FAVOURITE DRESS **********************************************************************
+MERGE INTO [Dress] AS target
+USING (
+    SELECT
+        (SELECT userID FROM [User] WHERE username = 'dummy user') AS userID,
+        (SELECT topID FROM [Tops] WHERE topName = 'sweetheart') AS topID,
+        (SELECT skirtID FROM skirts WHERE skirtName = 'mermaid') AS skirtID
+) AS source
+ON 1=0
+WHEN NOT MATCHED THEN
+    INSERT (userID, topID, skirtID)
+    VALUES (source.userID, source.topID, source.skirtID);
