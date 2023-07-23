c62167ed060d77807a7af639c8764a535f0f704f
Jesse van der Merwe <jesse@bbd.co.za>
2023-07-23 09:40:00 +0200

    Added temporary button for login

--- app/src/main/java/com/sayyestothedress/app/HomeController.java
+++ app/src/main/java/com/sayyestothedress/app/HomeController.java
@@ -4,9 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String viewHome(){
-        return "index";
+    @RequestMapping("/Login")
+    public String viewLogin(){
+        return "login";
     }
-    
 }
