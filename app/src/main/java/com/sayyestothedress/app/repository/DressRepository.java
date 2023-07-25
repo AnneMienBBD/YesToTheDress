d5e2dda631763311bed8a245d74370d24144cd6d
Oarabile Moima <oarabile@bbd.co.za>
2023-07-25 21:04:29 +0200

    By ID / USER

--- app/src/main/java/com/sayyestothedress/app/repository/DressRepository.java
+++ app/src/main/java/com/sayyestothedress/app/repository/DressRepository.java
@@ -4,6 +4,10 @@ import com.sayyestothedress.app.entity.Dress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface DressRepository extends JpaRepository<Dress, Integer> { 
+public interface DressRepository extends JpaRepository<Dress, Integer> {
+    Dress findByDressID(int dressID);
+    List<Dress> findByUserID(int userID);
 }
