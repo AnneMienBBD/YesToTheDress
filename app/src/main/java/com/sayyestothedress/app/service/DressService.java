62d6425af35150084d9533fb314d8b0e9b6e5af0
Karishma George <karishmag@bbd.co.za>
2023-07-26 12:18:07 +0200

    added favourite

--- app/src/main/java/com/sayyestothedress/app/service/DressService.java
+++ app/src/main/java/com/sayyestothedress/app/service/DressService.java
@@ -6,6 +6,12 @@ package com.sayyestothedress.app.service;
 import com.sayyestothedress.app.entity.Dress;
 import com.sayyestothedress.app.model.DressDTO;
 import com.sayyestothedress.app.repository.DressRepository;
+import com.sayyestothedress.app.repository.ShoesRepository;
+import com.sayyestothedress.app.repository.SkirtsRepository;
+import com.sayyestothedress.app.repository.SleevesRepository;
+import com.sayyestothedress.app.repository.TopsRepository;
+import com.sayyestothedress.app.repository.TrainRepository;
+import com.sayyestothedress.app.repository.VeilsRepository;
 
 import java.util.List;
 
@@ -17,6 +23,19 @@ public class DressService {
 
      @Autowired
     private DressRepository dressRepository;
+     @Autowired
+    private ShoesRepository shoesRepository;
+     @Autowired
+    private SkirtsRepository skirtsRepository;
+     @Autowired
+    private SleevesRepository sleevesRepository;
+     @Autowired
+    private TopsRepository topsRepository;
+     @Autowired
+    private TrainRepository trainRepository;
+     @Autowired
+    private VeilsRepository veilsRepository;
+
 
     public List<Dress> fetchDressList()
     {
@@ -29,13 +48,36 @@ public class DressService {
         return dressRepository.findByUserID(userID);
     }
 
-
     public Dress addDress(DressDTO dressDTO){
 
-        Dress dress;
+         Dress.DressBuilder dress = Dress.builder();
 
+        if (dressDTO.getShoes() != null){
+           dress.shoesID(shoesRepository.findFirstByShoeName(dressDTO.getShoes()).getShoesID());
+        }
 
-        return dressRepository.save(dress);
+        if (dressDTO.getSkirt() != null){
+            dress.skirtID(skirtsRepository.findFirstBySkirtName(dressDTO.getSkirt()).getSkirtID());
+        }
+
+        if (dressDTO.getSleeves() != null){
+           dress.sleeveID(sleevesRepository.findFirstBySleeveName(dressDTO.getSleeves()).getSleeveID());
+        }
+
+          if (dressDTO.getTop() != null){
+            dress.topID(topsRepository.findFirstByTopName(dressDTO.getTop()).getTopID());
+        }
+
+          if (dressDTO.getTrain() != null){
+            dress.trainID(trainRepository.findFirstByTrainName(dressDTO.getTrain()).getTrainID());
+        }
+        
+         if (dressDTO.getVeil() != null){
+          dress.veilID(veilsRepository.findFirstByVeilName(dressDTO.getVeil()).getVeilID());
+        }
+        
+        Dress saveDress = dress.build();
+        return dressRepository.save(saveDress);
     }
     
 }
