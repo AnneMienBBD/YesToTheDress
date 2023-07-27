60da114301a462dfb89d1ba9c81712ac263fd589
Mmasehume Raphiri <mmasehume@bbd.co.za>
2023-07-27 17:57:04 +0200

    landing page routing

--- app/src/main/java/com/sayyestothedress/app/config/SecurityConfig.java
+++ app/src/main/java/com/sayyestothedress/app/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable()
         .authorizeHttpRequests(authorize -> authorize
-        .antMatchers("/login", "/signup", "/js/**", "/css/**", "/images/**", "/home", "/favourites")
+        .antMatchers("/login", "/signup", "/js/**", "/css/**", "/images/**", "/home", "/favourites", "/**")
         .permitAll()
         .anyRequest().authenticated())
         .oauth2ResourceServer().jwt();
